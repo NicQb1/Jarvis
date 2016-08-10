237ceaa9ce59d9085f508823499e1baf6152886d
Nick Hutchison <n_hutchison@yahoo.com>
2016-08-09 20:56:00 -0400

    Base classes

---
+++ Jarvis/Graph_Database_Access/AccessClasses/WordAccess.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Graph_Database_Access.BusinessObjects;
+using Neo4jClient;
+using Graph_Database_Access.Relationships;
+
+namespace Graph_Database_Access.AccessClasses
+{
+    public class WordAccess : BaseAccess, IGDBAccess<Word>
+    {
+
+        public Word GetObjectClass(object value)
+        {
+
+           
+            var result =
+              client.Cypher.Match("(command:Command)")
+                  .Where((Word word) => word.word == (string)value)
+                  .Return(word => word.As<Word>())
+                  .Results
+                  .Single();
+            return result;
+        }
+
+      
+
+        public NodeReference<Word> InsertNode(Word myWord)
+        {
+           
+            var myNodeReference = client.Create(myWord);
+            PhraseAccess pa = new PhraseAccess();
+            var phrasenode = pa.InsertNode(pa.GetObjectClass(myWord.word));
+            
+            return myNodeReference;
+        }
+    }
---
+++ Jarvis/Graph_Database_Access/Relationships/PhraseCommandRelationship.cs
@@ -0,0 +1,29 @@
+﻿using Graph_Database_Access.BusinessObjects;
+using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graph_Database_Access.Relationships
+{
+    public class PhraseCommandRelationship : Relationship,
+        IRelationshipAllowingSourceNode<Command>,
+        IRelationshipAllowingTargetNode<Phrase>
+    {
+
+        public const string TypeKey = "Phrase_Means_Command";
+
+        public PhraseCommandRelationship(NodeReference targetNode) : base(targetNode)
+        {
+        }
+
+
+
+        public override string RelationshipTypeKey
+        {
+            get { return TypeKey; }
+        }
+    }
+}
